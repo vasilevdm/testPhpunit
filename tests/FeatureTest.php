0c0858ceb47e4750a67601fd56834a128d419834
D V <vasilev.dm@gmail.com>
2020-09-01 14:09:45 +0300

    update composer json

---
+++ tests/FeatureTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SimpleTest extends WebTestCase
+{
+    public function testFeature()
+    {
+        $this->assertTrue(false);
+    }
+}
