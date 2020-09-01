de7571cc31dd42b5350838567c3893197d4cb0dd
D V <vasilev.dm@gmail.com>
2020-09-01 12:15:29 +0300

    initial commit

---
+++ tests/SimpleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SimpleTest extends WebTestCase
+{
+    public function testSimple()
+    {
+        $this->assertTrue(true);
+    }
+}
